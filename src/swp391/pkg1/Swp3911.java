1767455c8eb82689c7d65dc5ad07bf378d906958
doit-now <hoang.nguyenthe@gmail.com>
2023-09-12 11:17:15 +0700

    upload 1st

---
+++ src/swp391/pkg1/Swp3911.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package swp391.pkg1;
+
+/**
+ *
+ * @author giao.lang
+ */
+public class Swp3911 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to Java, GitHub");
+    }
+    
+}
